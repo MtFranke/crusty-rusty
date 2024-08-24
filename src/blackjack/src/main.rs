615b9c56b2d12c61e2f7b5f92653758c50c07353
MtFranke <mateusz.franke@proton.me>
2024-08-24 11:41:49 +0200

    add hit/pass option with score count

--- src/blackjack/src/main.rs
+++ src/blackjack/src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::fmt;
+use std::{fmt, io};
 
 const ALL_SUITS: [Suit; 4] = [Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades];
 
@@ -13,13 +13,36 @@ fn main() {
     loop {
         let card = get_random_card_from_deck(&deck);
         let card_value = get_card_value(card);
-        println!("{} of {}, value: {} | {}", card.rank, card.suit, card_value);
+        println!("{} of {}, value: {}", card.rank, card.suit, card_value);
         score += card_value;
         if score > 21 {
             println!("You lost! You reached {score}");
             break;
         }
+
+        println!("Hit(h) or pass(p) | current value: {score}");
+        let next_card: bool = next_card();
+        println!("{}", next_card);
+        if next_card == false {
+            break;
+        }
     }
+
+    println!("You scored {score}!");
+}
+
+fn next_card() -> bool {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line");
+
+    if buffer.trim() == "h" {
+        println!("returning true");
+        return true;
+    }
+
+    false
 }
 
 fn get_random_card_from_deck(deck: &Vec<Card>) -> &Card {
