654b5140720f330711abef8f378f50640fccb48e
MtFranke <mateusz.franke@proton.me>
2024-08-26 16:23:17 +0200

    adjust blacjack logic

--- src/blackjack/src/main.rs
+++ src/blackjack/src/main.rs
@@ -5,18 +5,30 @@ fn main() {
     println!("Welcome to Blackjack game!, your goal is it hit 21!");
     let mut deck = cards::generate_deck();
     //list_deck(&deck);
-    let mut score: i32 = 0;
+    let mut player: i32 = 0;
+    let mut dealer = 0;
 
+    let card = cards::get_random_card_from_deck(&mut deck);
+    let card_value = cards::get_card_value(&card);
+    println!("Deler card");
+    println!("{} of {}, value: {}", card.rank, card.suit, card_value);
+    dealer += card_value;
     //init game with 2 random cards face up
+    println!("Player cards");
     for _ in 0..2 {
         let card = cards::get_random_card_from_deck(&mut deck);
         let card_value = cards::get_card_value(&card);
         println!("{} of {}, value: {}", card.rank, card.suit, card_value);
-        score += card_value;
+        player += card_value;
+    }
+
+    if (player == 21) {
+        println!("Player wins, blackjack!");
+        return;
     }
 
     loop {
-        println!("Hit(h) or stand(s) | current value: {score}");
+        println!("Hit(h) or stand(s) | current value: {player}");
         let next_card: bool = next_card();
         if next_card == false {
             break;
@@ -25,14 +37,42 @@ fn main() {
         let card = cards::get_random_card_from_deck(&mut deck);
         let card_value = cards::get_card_value(&card);
         println!("{} of {}, value: {}", card.rank, card.suit, card_value);
-        score += card_value;
-        if score > 21 {
-            println!("You lost! You reached {score}");
+        player += card_value;
+        if player > 21 {
+            println!("Bust! {player}");
             break;
         }
     }
 
-    println!("You scored {score}!");
+    if player > 21 {
+        return;
+    }
+
+    let card = cards::get_random_card_from_deck(&mut deck);
+    let card_value = cards::get_card_value(&card);
+    println!("Deler second card");
+    println!("{} of {}, value: {}", card.rank, card.suit, card_value);
+    dealer += card_value;
+
+    if dealer < 17 {
+        let card = cards::get_random_card_from_deck(&mut deck);
+        let card_value = cards::get_card_value(&card);
+        println!("Deler threed card");
+        println!("{} of {}, value: {}", card.rank, card.suit, card_value);
+        dealer += card_value;
+    }
+
+    if dealer > 21 {
+        println!("Dealer busts! {dealer}!");
+        println!("You won!");
+        return;
+    }
+
+    if player > dealer {
+        println!("You WON! against a dealer, player: {player}, dealer: {dealer}");
+    } else {
+        println!("You LOST! against a dealer, player: {player}, dealer: {dealer}");
+    }
 }
 
 fn next_card() -> bool {
